81c7fa0eeb95651997d79444f0c2f0e343deaf68
lawrencexfs <425769163@qq.com>
2024-07-10 17:39:35 +0800

    feat(thread):线程同步分析，关于条件变量通知唤醒线程。新增信号处理，安全退出。

--- cmd/threadpool/poolx.cpp
+++ cmd/threadpool/poolx.cpp
@@ -5,12 +5,32 @@
 #include <functional>
 #include <mutex>
 #include <condition_variable>
+#include <csignal>
+#include <cstdlib>
+
+// g++ -o demo poolx.cpp  -L/usr/lib -lssl -lcrypto -luuid -L/usr/local/lib -g -std=c++20 -pthread
 
 std::vector<std::thread> workers;
 std::queue<std::function<void()>> tasks;
 std::mutex queue_mutex;
 std::condition_variable cond_var;
 bool stop = false; // 控制线程退出的标记
+
+void signalHandler(int signal)
+{
+    std::cout << "Signal " << signal << " received." << std::endl;
+
+    // 停止所有线程
+    {
+        std::lock_guard<std::mutex> lock(queue_mutex);
+        stop = true;
+    }
+    cond_var.notify_all();
+    // 清理资源
+    // ...
+    // 退出程序
+    // std::exit(0);
+}
 
 void threadPoolTask()
 {
@@ -51,6 +71,9 @@ void addTask(std::function<void()> task)
 
 int main()
 {
+    // 设置信号处理器
+    std::signal(SIGINT, signalHandler); // SIGINT 通常由 Ctrl+C 触发
+
     int numThreads = 4; // 假设我们想要4个工作线程
     for (int i = 0; i < numThreads; ++i)
     {
@@ -62,6 +85,14 @@ int main()
             { std::cout << "Task 1 executed by thread\n"; });
     addTask([]()
             { std::cout << "Task 2 executed by thread\n"; });
+    addTask([]()
+            { std::cout << "Task 3 executed by thread\n"; });
+    addTask([]()
+            { std::cout << "Task 4 executed by thread\n"; });
+    addTask([]()
+            { std::cout << "Task 5 executed by thread\n"; });
+    addTask([]()
+            { std::cout << "Task 6 executed by thread\n"; });
 
     // 等待所有任务完成
     for (auto &th : workers)
@@ -71,13 +102,6 @@ int main()
             th.join();
         }
     }
-
-    // 停止所有线程
-    {
-        std::lock_guard<std::mutex> lock(queue_mutex);
-        stop = true;
-    }
-    cond_var.notify_all();
     std::cout << "Pool over\n";
 
     return 0;
